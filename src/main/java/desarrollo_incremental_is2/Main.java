2fef221f644b0648f42ed979d1b219f51289aac8
Cliente CONTROL-PC <CltControl@GGIECLB5WRK030.espol.edu.ec>
2019-12-16 16:32:56 -0500

    multiplicaicon implementada

--- src/main/java/desarrollo_incremental_is2/Main.java
+++ src/main/java/desarrollo_incremental_is2/Main.java
@@ -3,7 +3,8 @@ package desarrollo_incremental_is2;
 class Main {
 	public static void main(String[] args) {
 		Suma suma = new Suma();
-		Multiplicacion multiplicacion = new Multiplicacion();
+		Multiplicacion multiplicacion = new Multiplicacion(suma);
+		System.out.println(multiplicacion.multiplicar(5, -1));
 		Resta resta = new Resta();
 	}
 }
--- src/main/java/desarrollo_incremental_is2/Multiplicacion.java
+++ src/main/java/desarrollo_incremental_is2/Multiplicacion.java
@@ -1,11 +1,31 @@
 package desarrollo_incremental_is2;
 
 public class Multiplicacion {
-	public Multiplicacion() {
-		// TODO Auto-generated constructor stub
+	
+	public Suma suma;
+	
+	public Multiplicacion(Suma suma) {
+		this.suma=suma;
 	}
 	
-	public double multiplicar( double x, double y ) {
-		return 0;
+	public double multiplicar(double multiplicando, double multiplicador) 
+	{
+		double ret = Math.abs(multiplicando);
+		
+		for (int i=1; i< Math.abs(multiplicador);i++) 
+		{
+			ret=suma.sumar(Math.abs(multiplicando),ret);
+		}
+		/*Positivas*/
+		if(multiplicador>0) 
+		{
+			return ret;
+		}
+		/*Negativas*/
+		else 
+		{
+			return -ret;
+		}
+			
 	}
 }
